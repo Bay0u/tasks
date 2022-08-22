bbd1b04746d773cb60cd65f6d795ae7a07a865a7
Bay0u <a.bayoumimahmoud@gmail.com>
2022-08-22 17:57:05 +0200

    .

--- Movies-Catalog/my-new-app/src/app/components/login/auth.service.ts
+++ Movies-Catalog/my-new-app/src/app/components/login/auth.service.ts
@@ -11,17 +11,17 @@ export class AuthService {
    isUserLoggedIn: boolean = false;
 
    login(userName: string, password: string): Observable<boolean> {
-      console.log(userName);
-      console.log(password);
+      //console.log(userName);
+      //console.log(password);  
       this.isUserLoggedIn = userName == 'admin@ad.com' && password == 'admin';
       localStorage.setItem('isUserLoggedIn', this.isUserLoggedIn ? "true" : "false"); 
-
+      
    return of(this.isUserLoggedIn).pipe(
       delay(1000),
       tap(val => { 
          console.log("Is User Authentication is successful: " + val);
          if(!val){
-            alert("Wrong Input Please use admin to login");
+            alert("Wrong username or password Please use admin details to login         [little tip look bottom right the page]");
          }
       })
    );
